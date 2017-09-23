977554355f17fbdc3b8f637afb5f2318a994ebe3
Jennifer Kemp <jenn.kemp.codes@gmail.com>
2017-09-23 16:03:54 -0400

    Favorite Food and Favorite Drink Automated Response Script

---
+++ method_building_practice.rb
@@ -0,0 +1,29 @@
+def favorite_food (name)
+  if name == 'Jennifer'
+    return 'cheesecake'
+  end
+
+    if name == 'Cody'
+      return 'Pumpkin Spice'
+    end
+
+    'Oh Idk..maybe Saurkraut'
+  end
+
+  def favorite_drink name
+    if name == 'Jean'
+      'Earl-Gray'
+    elsif name == 'Kathryn'
+      'Coffee, on the rocks'
+    else
+      'ummm perhaps pina colada'
+    end
+  end
+
+  puts favorite_food('Jennifer')
+  puts favorite_food('Cody')
+  puts favorite_food('Cher')
+  puts favorite_drink('Jean')
+  puts favorite_drink('Kathryn')
+  puts favorite_drink('Jerry')
+
